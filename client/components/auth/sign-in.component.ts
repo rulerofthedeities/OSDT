cff2a104ea0ae6e105743869f3eef5f48e5dcd3b
rulerofthedeities <github_rulerofthedeities@yahoo.com>
2016-09-20 19:49:13 +0200

    Redesign Signin Form

--- client/components/auth/sign-in.component.ts
+++ client/components/auth/sign-in.component.ts
@@ -7,47 +7,93 @@ import {User} from '../../models/user.model';
 
 @Component({
   template: `
-    <section>
-      <form #f="ngForm" novalidate (ngSubmit)="onSubmit(f.value, f.valid)">
+    <div id="loginbox" style="margin-top:50px;" class="col-md-6 col-md-offset-3 col-sm-8 col-sm-offset-2">                    
+      <div class="panel panel-primary">
+        <div class="panel-heading">
+          <div class="panel-title">Sign In</div>
+          <div *ngIf="false" style="float:right; font-size: 80%; position: relative; top:-10px">
+            <a href="#">Forgot password?</a>
+          </div>
+        </div>     
+        <div style="padding-top:30px" class="panel-body bg-info">
+          <form #f="ngForm" 
+            id="loginform" 
+            class="form-horizontal" 
+            role="form" 
+            novalidate 
+            (ngSubmit)="onSubmit(f.value, f.valid)">
+            
+            <div class="input-group">
+              <span class="input-group-addon">
+                <i class="glyphicon glyphicon-user"></i>
+              </span>
+              <input id="email" 
+                type="text" 
+                class="form-control" 
+                name="email"
+                placeholder="Email address"
+                [ngModel]="user.email"
+                validateEmail
+                required  
+                #email="ngModel">                                       
+            </div>
+                
+            <small class="text-danger" [hidden]="!email?.errors?.required || (email?.pristine && !f.submitted)">
+              Email is required.
+            </small>
+            <small class="text-danger" [hidden]="!email?.errors?.invalidMail || (email?.pristine && !f.submitted)">
+              Email format should be <i>john@doe.com</i>.
+            </small> 
 
-        <div>
-          <label for="email">Mail</label>
-          <input 
-            type="email" 
-            class="form-control" 
-            name="email"
-            id="email"
-            [ngModel]="user.email"
-            validateEmail
-            required  
-            #email="ngModel">
-          <small [hidden]="!email?.errors?.required || (email?.pristine && !f.submitted)">
-            Email is required.
-          </small>
-          <small [hidden]="!email?.errors?.invalidMail || (email?.pristine && !f.submitted)">
-            Email format should be <i>john@doe.com</i>.
-          </small>
-          <!-- <pre *ngIf="email.errors" class="margin-20">{{ email.errors | json }}</pre> -->
-        </div>
-          
-        <div>
-          <label for="password">Password</label>
-          <input 
-            type="password" 
-            class="form-control" 
-            name="password" 
-            id="password" 
-            [ngModel]="user.password" 
-            required 
-            #password="ngModel">
-            <small [hidden]="!password?.errors?.required || (password?.pristine && !f.submitted)">
+            <div style="margin-top: 25px" class="input-group">
+              <span class="input-group-addon">
+                <i class="glyphicon glyphicon-lock"></i>
+              </span>
+              <input id="login-password" 
+                type="password" 
+                class="form-control" 
+                name="password" 
+                placeholder="Password"
+                [ngModel]="user.password" 
+                required 
+                #password="ngModel">
+            </div>
+                
+            <small class="text-danger" [hidden]="!password?.errors?.required || (password?.pristine && !f.submitted)">
               Password is required
             </small>
-          <!-- <pre *ngIf="password.errors" class="margin-20">{{ password.errors | json }}</pre> -->
-        </div>
-        <button type="submit" [disabled]="!f.valid">Sign In</button>
-      </form>
-    </section>`
+
+            <div style="margin-top: 25px" class="input-group">
+              <div class="checkbox">
+                <label>
+                  <input id="login-remember" type="checkbox" name="remember" value="1"> Remember me
+                </label>
+              </div>
+            </div>
+
+            <div style="margin-top:10px" class="form-group">
+                <div class="col-sm-12 controls">
+                  <button type="submit"
+                    class="btn btn-success"
+                    [disabled]="!f.valid">
+                    Sign In
+                  </button>
+                </div>
+            </div>
+
+            <div class="form-group">
+              <div class="col-md-12 control">
+                <div style="border-top: 1px solid#888; padding-top:15px; font-size:85%" >
+                  Don't have an account?
+                    <a routerLink="/auth/signup">Sign Up Here</a>
+                </div>
+              </div>
+            </div>    
+          </form>     
+        </div>                     
+      </div>
+    </div>
+  `
 })
 
 export class SignIn implements OnInit {
