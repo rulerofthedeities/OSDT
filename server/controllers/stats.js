88c15b6eb3e2d8cd297e935fccb1de3089b8dbab
rulerofthedeities <github_rulerofthedeities@yahoo.com>
2016-09-20 12:15:03 +0200

    Filter dashboard by userId

--- server/controllers/stats.js
+++ server/controllers/stats.js
@@ -5,19 +5,21 @@ var mongoose = require('mongoose'),
     async = require('async'),
     totals = {},
     totalsErr = null,
-    defaultCurrency = 'EUR';
+    defaultCurrency = 'EUR',
+    userId = null;
 
-var getRecipientCount = function(callback) {
-  Recipient.count({}, function(err, count) {
+var getRecipientCount = function(callback) {     
+  Recipient.count({userId: userId}, function(err, count) {
     callback(err, count);
   });
 }
 
 var getDonationsCount = function(callback) {
   var pipeline = [
-    {$unwind:'$donations'},
-    {$group:{_id:null,count:{$sum:1}}}
-  ];
+        {$match:{userId: userId}},
+        {$unwind:'$donations'},
+        {$group:{_id:null,count:{$sum:1}}}
+      ];
   Recipient.aggregate(pipeline, function(err, count) {
     callback(err, count[0].count);
   });
@@ -25,10 +27,11 @@ var getDonationsCount = function(callback) {
 
 var getDonationsPerCurrency = function(callback) {
   var pipeline = [
-    {$unwind:'$donations'},
-    {$group:{_id:'$donations.currency',count:{$sum:'$donations.amount'}}},
-    {$project:{_id:0, currency:'$_id', total:'$count'}}
-  ];
+        {$match:{userId: userId}},
+        {$unwind:'$donations'},
+        {$group:{_id:'$donations.currency',count:{$sum:'$donations.amount'}}},
+        {$project:{_id:0, currency:'$_id', total:'$count'}}
+      ];
   Recipient.aggregate(pipeline, function(err, totals) {
     callback(err, totals);
   });
@@ -36,10 +39,11 @@ var getDonationsPerCurrency = function(callback) {
 
 var getTotalAmount = function(callback) {
   var pipeline = [
-    {$unwind:'$donations'},
-    {$group:{_id:null, count:{$sum:'$donations.values.' + defaultCurrency}}},
-    {$project:{_id:0, total:'$count'}}
-  ];
+        {$match:{userId: userId}},
+        {$unwind:'$donations'},
+        {$group:{_id:null, count:{$sum:'$donations.values.' + defaultCurrency}}},
+        {$project:{_id:0, total:'$count'}}
+      ];
   Recipient.aggregate(pipeline, function(err, totals) {
     var total = totals[0] ? totals[0].total : 0;
     callback(err, {total:total, currency:defaultCurrency});
@@ -47,14 +51,15 @@ var getTotalAmount = function(callback) {
 }
 
 var getTotalAmountThisYear = function(callback) {
-  var lastYear = new Date().getFullYear() - 1;
-  var cutoff = new Date(lastYear, 11, 31);
-  var pipeline = [
-    {$unwind:'$donations'},
-    {$match: {'donations.dtPaid':{$gt:cutoff}}},
-    {$group:{_id:null, count:{$sum:'$donations.values.' + defaultCurrency}}},
-    {$project:{_id:0, total:'$count'}}
-  ];
+  var lastYear = new Date().getFullYear() - 1,
+      cutoff = new Date(lastYear, 11, 31),
+      pipeline = [
+        {$match:{userId: userId}},
+        {$unwind:'$donations'},
+        {$match: {'donations.dtPaid':{$gt:cutoff}}},
+        {$group:{_id:null, count:{$sum:'$donations.values.' + defaultCurrency}}},
+        {$project:{_id:0, total:'$count'}}
+      ];
   Recipient.aggregate(pipeline, function(err, totals) {
     var total = totals[0] ? totals[0].total : 0;
     callback(err, {total:total, currency:defaultCurrency});
@@ -62,16 +67,17 @@ var getTotalAmountThisYear = function(callback) {
 }
 
 var getTotalAmountLastYear = function(callback) {
-  var lastYear = new Date().getFullYear() - 1;
-  var prevYear = new Date().getFullYear() - 2;
-  var cutoff1 = new Date(lastYear, 11, 31);
-  var cutoff2 = new Date(prevYear, 11, 31);
-  var pipeline = [
-    {$unwind:'$donations'},
-    {$match: {$and:[{'donations.dtPaid':{$gt:cutoff2}}, {'donations.dtPaid':{$lt:cutoff1}}]}},
-    {$group:{_id:null, count:{$sum:'$donations.values.' + defaultCurrency}}},
-    {$project:{_id:0, total:'$count'}}
-  ];
+  var lastYear = new Date().getFullYear() - 1,
+      prevYear = new Date().getFullYear() - 2,
+      cutoff1 = new Date(lastYear, 11, 31),
+      cutoff2 = new Date(prevYear, 11, 31),
+      pipeline = [
+        {$match:{userId: userId}},
+        {$unwind:'$donations'},
+        {$match: {$and:[{'donations.dtPaid':{$gt:cutoff2}}, {'donations.dtPaid':{$lt:cutoff1}}]}},
+        {$group:{_id:null, count:{$sum:'$donations.values.' + defaultCurrency}}},
+        {$project:{_id:0, total:'$count'}}
+      ];
   Recipient.aggregate(pipeline, function(err, totals) {
     var total = totals[0] ? totals[0].total : 0;
     callback(err, {total:total, currency:defaultCurrency});
@@ -80,26 +86,28 @@ var getTotalAmountLastYear = function(callback) {
 
 var getRecentDonations = function(callback) {
   var pipeline = [
-    {$project: {donation:"$donations", "recipient":{id:"$_id", name:"$name"}}},
-    {$unwind: "$donation"},
-    {$sort: {"donation.dtPaid" : -1}},
-    {$limit:5},
-    {$project: {_id:0,donation:1,recipient:1}}
-  ];
+        {$match:{userId: userId}},
+        {$project: {donation:"$donations", "recipient":{id:"$_id", name:"$name"}}},
+        {$unwind: "$donation"},
+        {$sort: {"donation.dtPaid" : -1}},
+        {$limit:5},
+        {$project: {_id:0,donation:1,recipient:1}}
+      ];
   Recipient.aggregate(pipeline, function(err, donations) {
     callback(err, donations);
   });
 }
 
 var getTopDonations = function(callback) {
-  var sort = JSON.parse('{"donation.values.' + defaultCurrency + '":-1}');
-  var pipeline = [
-    {$project: {donation:"$donations", "recipient":{id:"$_id", name:"$name"}}},
-    {$unwind: "$donation"},
-    {$sort: sort},
-    {$limit:5},
-    {$project: {_id:0,donation:1,recipient:1}}
-  ];
+  var sort = JSON.parse('{"donation.values.' + defaultCurrency + '":-1}'),
+      pipeline = [
+        {$match:{userId: userId}},
+        {$project: {donation:"$donations", "recipient":{id:"$_id", name:"$name"}}},
+        {$unwind: "$donation"},
+        {$sort: sort},
+        {$limit:5},
+        {$project: {_id:0,donation:1,recipient:1}}
+      ];
   Recipient.aggregate(pipeline, function(err, donations) {
     callback(err, {donations:donations, currency: defaultCurrency});
   });
@@ -107,11 +115,12 @@ var getTopDonations = function(callback) {
 
 var getTopRecipients = function(callback) {
   var pipeline = [
-    {$unwind:'$donations'},
-    {$group:{_id:'$_id', totalAmount:{$sum:'$donations.values.' + defaultCurrency}, name:{$first: "$name"}}},
-    {$project:{totalAmount:1, name:1}},
-    {$sort: {totalAmount: -1}}
-  ]; 
+        {$match:{userId: userId}},
+        {$unwind:'$donations'},
+        {$group:{_id:'$_id', totalAmount:{$sum:'$donations.values.' + defaultCurrency}, name:{$first: "$name"}}},
+        {$project:{totalAmount:1, name:1}},
+        {$sort: {totalAmount: -1}}
+      ]; 
   Recipient.aggregate(pipeline, function(err, recipients) {
     callback(err, {recipients:recipients, currency: defaultCurrency});
   });
@@ -122,6 +131,7 @@ module.exports = {
   getTotals: function(req, res) {
     defaultCurrency = req.params.currency || defaultCurrency;
     defaultCurrency = defaultCurrency.toUpperCase();
+    userId = mongoose.Types.ObjectId(req.decoded.user._id);
     async.parallel({
       countRecipients: getRecipientCount,
       countDonations: getDonationsCount,
@@ -138,6 +148,7 @@ module.exports = {
   getLists: function(req, res) {
     defaultCurrency = req.params.currency || defaultCurrency;
     defaultCurrency = defaultCurrency.toUpperCase();
+    userId = mongoose.Types.ObjectId(req.decoded.user._id);
     async.parallel({
       topDonations: getTopDonations,
       recentDonations: getRecentDonations,
